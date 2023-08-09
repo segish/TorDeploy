adb077d13e42836a5a8cc48971b9d79a832deb5d
segish <tsegatigneh08@example.com>
2023-08-09 18:00:46 +0300

    thid commit

--- adminpanel/src/axios.js
+++ adminpanel/src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const makeRequest = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: "http://94.130.104.15/api/",
     withCredentials: true,
 })
