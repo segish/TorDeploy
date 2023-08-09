fecc89d30c395f1676fe402420704aace2a725f5
Tsega Tigneh <136186978+segish@users.noreply.github.com>
2023-08-09 18:11:25 +0300

    Update axios.js

--- adminpanel/src/axios.js
+++ adminpanel/src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const makeRequest = axios.create({
-    baseURL: "http://94.130.104.15/api/",
+    baseURL: "http://94.130.104.15/admin/api/",
     withCredentials: true,
 })
