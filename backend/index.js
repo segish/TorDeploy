cdd2475003d6403c392109250a6ed029ff60670e
segish <tsegatigneh08@example.com>
2023-08-09 23:49:20 +0300

    thid commit

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const cors = require ("cors");
+const cors = require("cors");
 const authRouter = require("./routes/Auth");
 const courseRouter = require("./routes/Courses");
 const InstructorRouter = require("./routes/Instructors");
@@ -12,20 +12,20 @@ const UserRouter = require("./routes/Users");
 const cookieParser = require("cookie-parser");
 
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL,{ 
-    useNewUrlParser: true, useUnifiedTopology: true 
-},).then(()=>console.log("Connected to MongoDB"))
-.catch((err)=>{console.log(" can't Connect to MongDB because of =>"+err)})
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+},).then(() => console.log("Connected to MongoDB"))
+    .catch((err) => { console.log(" can't Connect to MongDB because of =>" + err) })
 
 //midleware
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser());
 app.use(helmet());
 app.use(morgan("common"));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static('views'));
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
@@ -43,4 +43,4 @@ app.use("/api/instrutcors", InstructorRouter)
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
-  });
+});
