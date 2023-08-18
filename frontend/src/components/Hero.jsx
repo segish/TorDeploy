bc025e75083da9ad8aded25b529b1d4a1054dc75
segish <tsegatigneh08@example.com>
2023-08-18 17:48:50 +0300

    new update

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = () => {
   const { currentUser } = useContext(AuthContext)
   const { refreshLogin } = useContext(AuthContext)
   return (
-    <div id="home" className='w-full h-[70%] bg-gradient-to-r from-orange-800  flex flex-col justify-between'>
-      <div className='flex flex-col md:w-[600px] '>
-        <div className='flex flex-col rounded-lg bg-white dark:bg-black justify-center px-6 py-8 my-11 mx-12'>
+    <div id="home" className='w-full h-[65vh] bg-gradient-to-r from-orange-800  flex flex-col justify-between'>
+      <div className='flex '>
+        <div className='flex flex-col rounded-lg bg-white dark:bg-black justify-center px-[10%] p-[4%] my-[10%] mx-8 md:px-6 md:py-8 md:my-11 md:mx-12'>
           <p className='text-2xl font-bold dark:text-white text-center px-10 py-2'>Welcome to NERD's educational program.</p>
           <p className='text-2xl font-bold dark:text-white text-center px-3 py-3'>Quality education at your fingertips. Explore elearning today</p>
           {!currentUser&&<button className='border border-orange-600 w-[130px] py-3 my-4 mx-auto ' onClick={() => refreshLogin(true)}>Get Started</button>}
