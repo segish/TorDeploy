bfe488c48ad5f1345ef3159ea29187d8c55f7d37
segish <tsegatigneh08@example.com>
2023-08-18 13:00:09 +0300

    new update

--- frontend/src/components/MenuItems.js
+++ frontend/src/components/MenuItems.js
@@ -38,10 +38,10 @@ const MenuItems = ({ showMenu, active, setRegister }) => {
 
     <ul className={active ? 'absolute w-full  uppercase dark:bg-black bg-slate-900 px-8 lg:hidden z-[99999999999]' : 'hidden'}>
 
-      <li className='border-b-2 border-zinc-500 w-full' onClick={showMenu}><a href="/" className=' hover:text-orange-500  cursor-pointer'>Home</a></li>
-      <li className='border-b-2 border-zinc-500 w-full' onClick={showMenu}><a href="/#cors" className=' hover:text-orange-500 cursor-pointer'>Courses</a></li>
-      <li className='border-b-2 border-zinc-500 w-full' onClick={showMenu}><a href="/#inst" className=' hover:text-orange-500 cursor-pointer'>Our Communities</a></li>
-      <li className='border-b-2 border-zinc-500 w-full' onClick={showMenu}><Link to="/AboutUs" className=' hover:text-orange-500'>About Us</Link></li>
+      <li className='border-b-2 border-zinc-500 w-full' onClick={showMenu}><a href="/" className=' hover:text-orange-500 flex cursor-pointer w-full'>Home</a></li>
+      <li className='border-b-2 border-zinc-500 w-full' onClick={showMenu}><a href="/#cors" className=' hover:text-orange-500 flex cursor-pointer w-full'>Courses</a></li>
+      <li className='border-b-2 border-zinc-500 w-full' onClick={showMenu}><a href="/#inst" className=' hover:text-orange-500 flex cursor-pointer w-full'>Our Communities</a></li>
+      <li className='border-b-2 border-zinc-500 w-full' onClick={showMenu}><Link to="/AboutUs" className=' hover:text-orange-500 flex w-full'>About Us</Link></li>
       <div className='flex flex-col my-4'>
         {currentUser ?
           (<><button className='bg-transparent  text-white px-8 py-3 mb-4' onClick={showMenu}><PersonOutlineOutlinedIcon />{currentUser.username}</button>
